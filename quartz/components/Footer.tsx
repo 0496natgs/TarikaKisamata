662d680a9bba0ba210b1ab9d5d341b7010681d9d
0496natgs <edi.studio.design@gmail.com>
2024-07-08 20:12:12 -0600

    Quartz sync: Jul 8, 2024, 8:12 PM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -15,8 +15,8 @@ export default ((opts?: Options) => {
       <footer class={`${displayClass ?? ""}`}>
         <hr />
         <p>
-          {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
+          {i18n(cfg.locale).components.footer.createdBy}{" "}
+          <a href="https://quartz.jzhao.xyz/"> Natalia G. Sarmiento</a> © {year}
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
