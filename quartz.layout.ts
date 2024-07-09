b423ce5f840447400815f08690a03089e0783064
0496natgs <edi.studio.design@gmail.com>
2024-07-08 21:42:07 -0600

    Quartz sync: Jul 8, 2024, 9:42 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -33,7 +33,7 @@ export const defaultContentPageLayout: PageLayout = {
     // Component.DesktopOnly(Component.Backlinks()),
   ],
   right: [
-    // Component.Graph(),
+    Component.Graph(),
   ],
 }
 // components for pages that display lists of pages  (e.g. tags or folders)
